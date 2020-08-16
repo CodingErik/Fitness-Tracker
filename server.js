2d0da78b9b6bc25957194fb643728b4b07213d7b
Erik De Luna <codingerik1.gmail.com>
2020-08-16 10:54:08 -0500

    set up correct routes on the back end to be requested by the front end

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 // added orm to handle request database
 const mongoose = require("mongoose");
 // require path 
-const path = require("path"); 
+const path = require("path");
 
 // shortcicuit the port 
 const PORT = process.env.PORT || 3000;
@@ -36,62 +36,66 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 app.get('/exercise', (req, res) => {
     // send user to the index of the page 
     res.sendFile(path.join(__dirname, "./public/exercise.html"))
-}); 
+});
 
 
 // stats page
 app.get('/stats', (req, res) => {
     // send user to the index of the page 
     res.sendFile(path.join(__dirname, "./public/stats.html"))
-}); 
+});
 
 
 // API ROUTES 
-// **********************************************
-app.post("/submit", ({body}, res) => {
-    db.Book.create(body)
-      .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-      .then(dbLibrary => {
-        res.json(dbLibrary);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  app.get("/books", (req, res) => {
-    db.Book.find({})
-      .then(dbBook => {
-        res.json(dbBook);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  app.get("/library", (req, res) => {
-    db.Library.find({})
-      .then(dbLibrary => {
-        res.json(dbLibrary);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  app.get("/populated", (req, res) => {
-    db.Library.find({})
-      .populate("books")
-      .then(dbLibrary => {
-        res.json(dbLibrary);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+// **********************************************  
+// get all the workouts 
+app.get("/api/workouts", (req, res) => {
+    // db.Book.find({})
+    //     .then(dbBook => {
+    //         res.json(dbBook);
+    //     })
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
+});
+
+// update a specific workout 
+app.put("/api/workouts/:id", (req, res) => {
+    // db.Library.find({})
+    //     .then(dbLibrary => {
+    //         res.json(dbLibrary);
+    //     })
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
+});
+
+// create a new workout 
+app.post("/api/workouts", (req, res) => {
+    // db.Library.find({})
+    //     .populate("books")
+    //     .then(dbLibrary => {
+    //         res.json(dbLibrary);
+    //     })
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
+});
+
+// get a range of the workouts 
+app.get("/api/workouts", (req, res) => {
+    // db.Book.find({})
+    //     .then(dbBook => {
+    //         res.json(dbBook);
+    //     })
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
+});
+
 
 
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
